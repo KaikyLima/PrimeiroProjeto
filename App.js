70551e47a4d4b51070c5bf1151b42b53e9153396
Kaikyl <kaikylimasouza@gmail.com>
2023-08-11 20:04:32 -0300

    tde

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import CatFacts from './src/screens/CatFacts';
+import CardFacts from './src/components/CardFacts';
 
 export default function App() {
 
   
   return (
-    <CatFacts/>
+    <CardFacts/>
   );
 }
--- src/components/CardFacts.jsx
+++ src/components/CardFacts.jsx
@@ -1,30 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { TouchableOpacity, View, StyleSheet, Text, SafeAreaView } from "react-native";
+import axios from "axios";
+
+const style = StyleSheet.create({
+  cardContainer: {
+    backgroundColor: "#ccc",
+    marginTop: 300
+  },
+  cardTitle: {
+    color: "#000",
+  },
+});
 
 const CardFacts = () => {
+  // Usar um objeto como estado
+  const [cardData, setCardData] = useState({ fact: "", length: 0 });
 
-    const[cardFact, setCardFact] = useState ()
-
-    const fetchCardFact = async () => {
-        try{
-            const {data} = await axios.get('https://catfact.ninja/fact')
-            setCardFact(data.fact)
-        }catch(error){
-            console.log(error)
-        }
+  const fetchCardFact = async () => {
+    try {
+      const { data } = await axios.get("https://catfact.ninja/fact");
+      // Atualizar o estado com o fato e o comprimento
+      setCardData({ fact: data.fact, length: data.length });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const MyCard = ({onTouchCard}){
-        <TouchableOpacity onPress={onTouchCard}>
-            <View style={styles.cardContainer}>
-                <Text style={styles.cardTitle}>{ cardFact? cardFact: "" }</Text>
-            </View>
-        </TouchableOpacity>
-        }
+  useEffect(() => {
+    fetchCardFact();
+  }, []);
 
-
+  const MyCard = ({ onTouchCard }) => {
     return (
-        <MyCard/>
+      <SafeAreaView>
+        <TouchableOpacity onPress={onTouchCard}>
+          <View style={style.cardContainer}>
+            {/* Mostrar o fato e o comprimento no texto */}
+            <Text style={style.cardTitle}>
+              {cardData.fact ? `${cardData.fact} (${cardData.length} caracteres)` : ""}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </SafeAreaView>
     );
-}
+  };
+
+  return <MyCard onTouchCard={fetchCardFact} />;
+};
 
 export default CardFacts;
